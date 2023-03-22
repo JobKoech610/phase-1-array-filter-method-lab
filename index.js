b4c02642c3fb5aad585df700cfe3d4909e6efea0
Job koech <jobkoech610@gmail.com>
2023-03-22 17:03:33 +0300

    complted assignement

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // Code your solution here
 function findMatching(drivers, name) {
     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    
+
+    // return drivers.filter(function (driver) {
+    //   driver.toLowerCase() === name.toLowerCase()
+    // })
   }
+  
   
 
   function fuzzyMatch(drivers, query) {
-    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+    //startsWith() method determines whether a string begins with the characters of a specified string, returning true or false
+    return drivers.filter(driver => driver.startsWith(query));
+
   }
   
   function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+    //check if there is a name same the one give 
+    return drivers.filter(driver => driver.name === name);
   }
   
